cc0c1d98ad5e9d75cb45c689fed77631c8fb628e
khushijat17 <harekrishna.khushijat17@gmail.com>
2026-09-05 21:02:12 +0530

    Delete starpattern5.py

--- starpattern5.py
+++
@@ -1,8 +0,0 @@
-#Print Rhombus 
-n = int(input("Enter value: "))
-for i in range (1,n+1):
-		for j in range (n-i):
-				print(" ",end="")
-		for j in range (1,n+1):
-				print("*",end="")
-		print()
