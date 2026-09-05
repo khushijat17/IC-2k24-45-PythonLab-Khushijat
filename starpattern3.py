d5c6a3d8e5f532a180975b7c33a8751f1f9625d3
khushijat17 <harekrishna.khushijat17@gmail.com>
2026-09-05 21:00:48 +0530

    Delete starpattern3.py

--- starpattern3.py
+++
@@ -1,8 +0,0 @@
-#inverted triangle
-n = int(input("Enter value: "))
-for i in range (1,n+1):
-		for j in range(i-1):
-				print(" ",end="")
-		for j in range(n-i+1):
-				print("* ",end="")
-		print()
